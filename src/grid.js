f56e7e0fff9a2e599b4713e6811283167bb310e2
mansi-eng <mansi.paunikar19@vit.edu>
2021-05-02 14:50:44 +0530

    created grid

---
+++ src/grid.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const grid = () => {
+    return (
+      <div>
+        <button>1</button>
+        <button>1</button>
+        <button>1</button>
+        <button>1</button>
+      </div>
+     
+    );
+};
+
+export default grid;
